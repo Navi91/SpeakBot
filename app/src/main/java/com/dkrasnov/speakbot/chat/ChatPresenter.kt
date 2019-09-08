05b2b715d8b6db8e8c5b8faa506366cc51cd7944
dkrasnov <navi91@inbox.ru>
2019-09-08 21:21:56 +0300

    Added record.

--- app/src/main/java/com/dkrasnov/speakbot/chat/ChatPresenter.kt
+++ app/src/main/java/com/dkrasnov/speakbot/chat/ChatPresenter.kt
@@ -1,9 +1,11 @@
 package com.dkrasnov.speakbot.chat
 
+import android.content.Context
 import android.media.MediaRecorder
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.dkrasnov.speakbot.di.ComponentHolder
+import com.dkrasnov.speakbot.extensions.createAudoMessageFilePath
 import com.dkrasnov.speakbot.extensions.log
 import com.dkrasnov.speakbot.preferences.Preferences
 import java.io.IOException
@@ -13,9 +15,12 @@ import javax.inject.Inject
 class ChatPresenter : MvpPresenter<IChatView>() {
 
     @Inject
+    lateinit var context: Context
+    @Inject
     lateinit var preferences: Preferences
 
     private var recorder: MediaRecorder? = null
+    private var audioFilePath: String? = null
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -30,10 +35,16 @@ class ChatPresenter : MvpPresenter<IChatView>() {
     }
 
     fun onStartRecord() {
+        log("on start record")
+
+        audioFilePath = context.createAudoMessageFilePath(preferences.getAndIncrementUserMessageInde())
+
+        log("audio file path: $audioFilePath")
+
         recorder = MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile(MessageHelper.getAudioFileNameFromIndex(preferences.getAndIncrementUserMessageInde()))
+            setOutputFile(audioFilePath)
             setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
 
             try {
@@ -47,6 +58,8 @@ class ChatPresenter : MvpPresenter<IChatView>() {
     }
 
     fun onStopRecord() {
+        log("on stop record")
+
         recorder?.apply {
             stop()
             release()
--- app/src/main/java/com/dkrasnov/speakbot/extensions/ContextExtensions.kt
+++ app/src/main/java/com/dkrasnov/speakbot/extensions/ContextExtensions.kt
@@ -1,2 +1,12 @@
 package com.dkrasnov.speakbot.extensions
 
+import android.content.Context
+import java.io.File
+
+fun getAudioFileNameFromIndex(index: Int): String {
+    return "user_audio_message_$index"
+}
+
+fun Context.createAudoMessageFilePath(index: Int): String {
+    return File(this.externalCacheDir, getAudioFileNameFromIndex(index)).path
+}
